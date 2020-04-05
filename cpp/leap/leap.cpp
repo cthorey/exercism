b77415e030f68cb92d4ad6b6d8665b00b143e08a
cthorey <email@xihelm.com>
2020-04-05 13:09:26 +0200

    [FIX]update

--- cpp/leap/leap.cpp
+++ cpp/leap/leap.cpp
@@ -3,18 +3,7 @@
 namespace leap {
 
   bool is_leap_year(int year) {
-    bool result=false;
-    if (year % 4 == 0) {
-      if ( year % 100 == 0 && year % 400 !=0) {
-        result = false;
-      } else {
-        result = true;
-      }
-    }
-    else {
-      result = false;
-    }
-    return result;
+    return (year % 4 == 0 && year % 100 !=0 ) || (year % 400 == 0);
   }
 
 
